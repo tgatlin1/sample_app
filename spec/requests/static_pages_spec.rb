0927edd961bf85fafa266f7252fa88918818b106
tgatlin1 <toddgatlin2650@gmail.com>
2013-01-10 21:47:03 -0600

    final-final sample_app

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,23 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  
+
+
   describe "Home page" do
 
-    it "should have the h1 'Home'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Home')
+      page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', :text => " Home")
+      page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should not_have_selector('title', :text => ' Home')
     end
   end
   
